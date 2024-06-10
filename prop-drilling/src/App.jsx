e56ccf16ec2e00dd9cce9e14417981072ef559eb
praneet <praneetkashyap001@gmail.com>
2024-06-10 11:04:25 +0530

    first commit

---
+++ prop-drilling/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react"
+
+function App() {
+  const [count, setCount] = useState(0);
+  
+  return (
+    <div>
+      <Count count={count} />
+      <Buttons />
+    </div>
+  )
+}
+
+function Count({count}) {
+  return <div>
+    {count}
+  </div>
+}
+
+function Buttons() {
+  return <div>
+    <button onClick={() => {
+
+    }}>Increase</button>
+
+    <button onClick={() => {
+
+    }}>Decrease</button>
+  </div>
+}
+
+export default App
